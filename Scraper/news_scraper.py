cdb61a8c322eeaa8b5fe393fb31e73d218e3e3e4
Siyam Ahmed <siyamraisa@gmail.com>
2021-01-06 21:11:41 +0600

    Starting scraping activities

---
+++ Scraper/news_scraper.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+
+class Scraper:
+    def __init__(self):
+        self.headers_list = [
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13",
+            "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
+            "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+            "Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre",
+        ]
+
+        self.news_urls = {
+            "kalerkantha": "https://www.kalerkantho.com/print-edition/first-page"
+        }
+
+    def get_response(self, url):
+        session = requests.Session()
+        HEADER = {
+            "User-Agent": random.choice(self.headers_list),
+            "X-Requested-With": "XMLHttpRequest",
+            "Accept-Language": "bn",
+        }
+        session.headers.update(HEADER)
+        response = session.get(url)
+        return response
+
+    def scrape_kalerkantha(self):
+        response = self.get_response(self.news_urls["kalerkantha"])
+        soup = BeautifulSoup(response.content, "html.parser")
+        top_news_div = soup.find("div", {"class": "col-xs-12 top_news"})
+        mid_news_div = soup.find("div", {"class": "col-xs-12 mid_news"})
